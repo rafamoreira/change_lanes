7202b6d54c2361a17460f6468e38a3583ef6231a
Ewerton <shakamaiden@gmail.com>
2019-08-25 16:37:18 +0200

    Add Score and life to Scene

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,22 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int hearts;
+    public GameObject heart_output;
+    TextMeshProUGUI text_hearts;
 
     void Start() {
-	isAlive = true;
+	text_hearts = heart_output.GetComponent<TextMeshProUGUI>();
 	hearts = 3;
+	text_hearts.text = "Life: " + hearts;
     }
 
     public void TakeHit() {
         hearts--; 
-
         if (hearts <= 0) {
            GameManager.Instance.GameOver();
-	   isAlive = false;
         }
+	text_hearts.text = "Life: " + hearts;
     }
 }
--- Assets/Scripts/Player/PlayerScore.cs
+++ Assets/Scripts/Player/PlayerScore.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerScore : MonoBehaviour {
     public int score;
+    public GameObject score_output;
+    TextMeshProUGUI text_score;
 
+    void Start(){
+	text_score = score_output.GetComponent<TextMeshProUGUI>();
+	text_score.text = "Score: " + score;
+    }
+    
     public void GetBonus() {
-	Debug.Log("POINT");
         score += Mathf.RoundToInt(score * 0.1f);
+	score ++;
+
+	text_score.text = "Score: " + score;
     }
 }
