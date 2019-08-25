ff16f84e1d5f9044dbf028941f779015bd8b928c
Ewerton <shakamaiden@gmail.com>
2019-08-25 19:07:37 +0200

    Add Prerenderers

--- Assets/Scripts/MusicRythm.cs
+++ Assets/Scripts/MusicRythm.cs
@@ -27,24 +27,23 @@ public class MusicRythm : MonoBehaviour
 	//This event will be called every frame while music is playing
 	void onSpectrum (float[] spectrum)
 	{
-		//The spectrum is logarithmically averaged
-		//to 12 bands
+	    //The spectrum is logarithmically averaged
+	    //to 12 bands
         foreach(GameObject mo in MusicSpawner.Instance.musicList){
             for (int mli = 0; mli < 2; ++mli){
                 Transform curCubeList = mo.transform.GetChild(mli);
 
                 for (int i = 0; i < spectrum.Length; ++i) {
                     Transform curCube = curCubeList.GetChild(i);
-                    curCube.localScale = new Vector3(curCube.localScale.x, spectrum[i] * 30, curCube.localScale.z);        
+		    if (spectrum[i] < 0.01f){
+			curCube.localScale = new Vector3(curCube.localScale.x, spectrum[i] * 200, curCube.localScale.z);
+		    }
+		    else{
+			curCube.localScale = new Vector3(curCube.localScale.x, spectrum[i] * 20, curCube.localScale.z);        			
+		    }
+
                 }
             }
-            // foreach(Transform tm in mo.transform.getChild(2)){
-            //     Transform mc = tm.getChild(12);
-            //         }
-            //     Vector3 start = new Vector3 (i, 0, 0);
-            //     
-            //     Vector3 end = new Vector3 (i, spectrum [i], 0);
-            //     Debug.DrawLine (start, end);
 	    }
 	}
 }
