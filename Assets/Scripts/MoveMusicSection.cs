fa1dc243aacd726b62dcc26deca525a8b93a527c
Rafael Moreira <rafael@rafaelmoreira.org>
2019-08-25 16:03:22 -0300

    wave changes

--- Assets/Scripts/MoveMusicSection.cs
+++ Assets/Scripts/MoveMusicSection.cs
@@ -6,16 +6,25 @@ public class MoveMusicSection : MonoBehaviour
 {
     Rigidbody rbd;
     public float speed = 2;
+    float lifeTime;
     // Start is called before the first frame update
     void Start()
     {
+        lifeTime = 27;
         rbd = GetComponent<Rigidbody>();
-	rbd.velocity = new Vector3(0, 0, speed);   
+	    rbd.velocity = new Vector3(0, 0, speed);   
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
+    void Update(){
+        if (lifeTime >= 0){
+            lifeTime -= Time.deltaTime;
+        } else {
+            Despawn();
+        }
+    }
 
+    void Despawn(){
+        MusicSpawner.Instance.musicList.Remove(gameObject);
+        Destroy(gameObject);
     }
 }
