4cf72c12baebd85aa4d595ac1ac073f3838732a7
Ewerton <shakamaiden@gmail.com>
2019-08-25 04:01:54 +0200

    Initial Commit

---
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    public float speed = 10;
+    Rigidbody rbd;
+    
+    // Start is called before the first frame update
+    void Start() {
+        rbd = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        rbd.AddForce(0, 0, -speed , ForceMode.Impulse);
+    }
+}
